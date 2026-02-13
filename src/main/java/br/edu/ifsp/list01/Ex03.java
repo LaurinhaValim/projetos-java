bcba5438a8c8b447669faedfa6c6f93b954deaa5
Laura Valim <laura.v@aluno.ifsp.edu.br>
2026-02-13 13:12:31 -0300

    Update Ex03.java

--- src/main/java/br/edu/ifsp/list01/Ex03.java
+++ src/main/java/br/edu/ifsp/list01/Ex03.java
@@ -17,15 +17,15 @@ package br.edu.ifsp.list01;
 public class Ex03 {
 
     public static void main(String[] args) {
-        //Leia o input
-        //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
-        //Escreva o resultado da chamada do método compute() aqui
+        final Scanner scanner = new Scanner(System.in);
+        final int input = scanner.nextInt();
+        final Ex03 ex03 = new Ex03();
+        System.out.println(ex03.compute(input));
     }
 
-
     String compute(int input) {
-        String output = null;
-        //put your logic here
-        return output;
+        if (input <= 0) return "Erro";
+        if ((input % 400 == 0 ) || (input % 4 == 0 && input % 100 != 0)) return "Ano bissexto";
+        else return "Ano nao bissexto";
     }
 }
