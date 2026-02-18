ba32f52f6f86e176fb359739868f6aabc1e2d4be
Laura Valim <laura.v@aluno.ifsp.edu.br>
2026-02-18 20:25:38 -0300

    Resolução do Ex08

--- src/main/java/br/edu/ifsp/list01/Ex08.java
+++ src/main/java/br/edu/ifsp/list01/Ex08.java
@@ -1,5 +1,7 @@
 package br.edu.ifsp.list01;
 
+import java.util.Scanner;
+
 /*
     Alan quer comprar limões para fazer uma limonada. Próximo da sua casa há um mercadinho que vende limões da seguinte
     forma: O primeiro limão é vendido por C centavos, o segundo por C − 1 centavos, o terceiro por C − 2 e assim
@@ -22,14 +24,20 @@ package br.edu.ifsp.list01;
 public class Ex08 {
 
     public static void main(String[] args) {
-        //Leia o input
-        //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
-        //Escreva o resultado da chamada do método compute() aqui
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int c = scanner.nextInt();
+        final Ex08 ex08 = new Ex08();
+        System.out.println(ex08.compute(n, c));
     }
 
     int compute(int n, int c) {
-        int output = -1;
-        //put your logic here
-        return output;
+        int valorT = 0;
+        int preco = c;
+        for(int i = 0; i < n; i++){
+            valorT += preco;
+            if (preco > 1) preco--;
+        }
+        return valorT;
     }
 }
